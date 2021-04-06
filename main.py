506031868cebede90a8413a2bdf7d9afa0298eb6
Michael-Owen-19 <68501676+Michael-Owen-19@users.noreply.github.com>
2021-04-06 23:40:19 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import file_input as fi
+import ITB_MAP as fi
 import Graph as graf
 import fungsi as f
 import aStar as algo
@@ -9,6 +9,13 @@ listNode = []
 listCoordinate = []
 adjacencyMatrix = []
 
+## Fungsi untuk validasi node
+def isInNode(Node, listNodes):
+	for i in range (len(listNodes)):
+		if (listNodes[i]==Node):
+			return True
+	return False
+	
 ## Pilih File Input
 ## Mungkin nanti bisa kita buat if buat pilihan petanya
 
@@ -22,8 +29,19 @@ adjacencyMatrix = fi.adjacencyMatrix
 g = graf.Graph(numNode,listNode,listCoordinate,adjacencyMatrix)
 
 start = input("Masukkan node start: ")
+isIn=isInNode(start, listNode)
+while(isInNode(start, listNode)==False):
+	print("Node Tidak ditemukan silakan input ulang")
+	start = input("Masukkan node tujuan: ")
+	isIn=isInNode(start, listNode)
+	
 goal = input("Masukkan node tujuan: ")
-
+isIn=isInNode(goal, listNode)
+while(isIn==False):
+	print("Node Tidak ditemukan silakan input ulang")
+	goal = input("Masukkan node tujuan: ")
+	isIn=isInNode(goal, listNode)
+	
 listIdxAnswer = algo.aStar(g,g.getIdxNode(start),g.getIdxNode(goal))
 print(listIdxAnswer)
 listNodeAnswer = g.convertIdxToNodeList(listIdxAnswer)
