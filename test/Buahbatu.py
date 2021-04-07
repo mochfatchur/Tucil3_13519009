8c194cdbb3418cbbd08023b369a824a09d409215
mochfatchur <fathurmuhammad72@gmail.com>
2021-04-07 09:12:09 +0700

    benerin dikit

--- test/Buahbatu.py
+++ test/Buahbatu.py
@@ -1,8 +1,8 @@
 ## PETA Buahbatu
 numNode = 8
 
-latitude=-6.947766470635641
-longitude=107.65586801394181
+latitude = -6.948021406199452
+longitude = 107.65561722740172
 
 listKoordinat = [[-6.948021406199452, 107.65561722740172],
 		    [-6.9476480695358065, 107.65565807991213],
@@ -13,14 +13,14 @@ listKoordinat = [[-6.948021406199452, 107.65561722740172],
 		    [-6.948402149724846, 107.65584082025177],
 		    [-6.948209037854483, 107.65700094013205]
                  ]
-listNode = [["Jalan Neptunus Barat II - III"],
-	       ["Jalan Neptunus Barat III - IV"],
-	       ["Jalan Neptunus Barat IV - Neptunus Raya Barat"],
-	      l ["Jalan Neptunus Barat VI - Neptunus Raya Barat"],
-	       ["Jalan Neptunus Barat III - Neptunus Raya Barat"],
-	       ["Jalan Neptunus Barat II - Neptunus Barat - Neptunus Tengah"],
-	       ["Jalan Neptunus Barat I - Neptunus Barat - Neptunus Raya"],
-	       ["Jalan Neptunus Tengah II - Neptunus Tengah"],
+listNode = ["Jalan Neptunus Barat II - III",
+	    "Jalan Neptunus Barat III - IV",
+	    "Jalan Neptunus Barat IV - Neptunus Raya Barat",
+            "Jalan Neptunus Barat VI - Neptunus Raya Barat",
+	    "Jalan Neptunus Barat III - Neptunus Raya Barat",
+	    "Jalan Neptunus Barat II - Neptunus Barat - Neptunus Tengah",
+	    "Jalan Neptunus Barat I - Neptunus Barat - Neptunus Raya",
+	    "Jalan Neptunus Tengah II - Neptunus Tengah",
             ]
 
 adjacencyMatrix = [[0,1,0,0,0,1,0,0],
